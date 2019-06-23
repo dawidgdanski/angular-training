10026dd158bb2dd26ee19c90e658600a618a51c6
Dawid Gdański <davidgdanski@gmail.com>
2019-06-23 02:00:34 +0200

    Add Zippy component

---
+++ src/app/zippy/zippy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zippy',
+  templateUrl: './zippy.component.html',
+  styleUrls: ['./zippy.component.less']
+})
+export class ZippyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
