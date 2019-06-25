d7cb01d56ef0c9095692b8f66479aa8fd5387a96
Dawid Gdański <davidgdanski@gmail.com>
2019-06-26 00:47:01 +0200

    Use ng-content to copy the contents from the selector

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,24 +1,16 @@
-import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.less']
 })
-export class ZippyComponent implements AfterViewInit {
+export class ZippyComponent {
 
   @Input()
   title: string;
 
-  @ViewChild('summary', {read: ElementRef})
-  summaryText: ElementRef;
+  constructor() {
 
-  constructor(private element: ElementRef) {
-
-  }
-
-  ngAfterViewInit(): void {
-    console.log(this.element.nativeElement.innerText);
-    this.summaryText.nativeElement.innerText = this.element.nativeElement.innerText
   }
 }
