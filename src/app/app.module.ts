357b84b65c0e25c3c25b38ba5c85e3df89e4ae55
Dawid Gdański <davidgdanski@gmail.com>
2019-07-04 00:12:56 +0200

    Add ExampleFormComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {ZippyComponent} from './zippy/zippy.component';
 import {LikeComponent} from './like/like.component';
+import { ExampleFormComponent } from './example-form/example-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ZippyComponent,
-    LikeComponent
+    LikeComponent,
+    ExampleFormComponent
   ],
   imports: [
     BrowserModule
