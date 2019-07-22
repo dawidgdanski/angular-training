1a8fb07e3f50cd5d9e28e3ff14a546a918bfdab6
Dawid Gdański <davidgdanski@gmail.com>
2019-07-22 23:06:23 +0200

    Add custom validator

--- src/app/password-change/password-change.component.ts
+++ src/app/password-change/password-change.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {PasswordValidators} from './password.validators';
 
 @Component({
   selector: 'password-change',
@@ -19,7 +20,7 @@ export class PasswordChangeComponent implements OnInit {
   constructor(fb: FormBuilder) {
     this.form = fb.group({
       oldPassword: ['', [Validators.required, Validators.minLength(5)]],
-      newPassword: ['', Validators.required],
+      newPassword: ['', [Validators.required, PasswordValidators.cannotContainSpace]],
       confirmPassword: ['', Validators.required]
     });
   }
