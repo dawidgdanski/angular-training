0b2395ad82e22e86e4b06681d831444ef0fa6ee3
Dawid Gdański <davidgdanski@gmail.com>
2019-08-02 00:55:05 +0200

    Add NotFoundComponent, router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {PasswordChangeComponent} from './password-change/password-change.component';
 import {NewCourseFormComponent} from './new-course-form/new-course-form.component';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {RouterModule, Routes} from "@angular/router";
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {path: 'zippy', component: ZippyComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
@@ -19,11 +26,13 @@ import {NavBarComponent} from './nav-bar/nav-bar.component';
     PasswordChangeComponent,
     NewCourseFormComponent,
     NavBarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
